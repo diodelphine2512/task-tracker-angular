263fecdfab3e5099d9a7812f310714b443f31b1c
Evan <evanswartz01@gmail.com>
2022-02-02 16:36:01 -0500

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
