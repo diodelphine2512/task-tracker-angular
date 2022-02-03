9c0ad261738f870382b388e11ed899f1898cc846
Evan <evanswartz01@gmail.com>
2022-02-03 12:59:17 -0500

    added and deleted tasks now persist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
 
 
 @NgModule({
@@ -19,14 +22,15 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-
+    AddTaskComponent
 
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
 
   ],
   providers: [],
