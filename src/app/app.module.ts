5f599c2d03766a0d1a04f55bacb5aadd3374e76f
Evan <evanswartz01@gmail.com>
2022-02-03 10:41:29 -0500

    Added task-item and tasks components as well as services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,24 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
 
 
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
 
   ],
   providers: [],
